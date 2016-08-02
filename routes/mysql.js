13705756facdfdb8666ea601fde60e32a34b15e4
Harish Bansal <harishbansal1979@yahoo.com>
2016-08-02 13:02:50 +0530

    Added support for filtering

--- routes/mysql.js
+++ routes/mysql.js
@@ -42,7 +42,8 @@ exports.getConversations = function(req, res, next) {
 }
 
 exports.getSearchInfo = function(req, res, next) {
-  mysql.open(getConnectionParams(), function(err, conn) {
+  var conn = mysql.createConnection(getConnectionParams());
+  conn.connect(function(err) {
     if (err) return console.log(err);
     if(req.body.supportAgent)
     var supportAgent =  req.body.supportAgent.SUPPORT_ID;
@@ -52,46 +53,49 @@ exports.getSearchInfo = function(req, res, next) {
        
     
      if(supportAgent==="All" || supportAgent=="" || supportAgent==undefined)
-    	 condition += " 1=1";
+       condition += " 1=1";
      else
-       	 condition += " SUPPORT_ID="+ supportAgent; 
+         condition += " SUPPORT_ID="+ supportAgent; 
 
      if(dateCategory && dateCategory !=='All'){
-	    	
-	     if(dateCategory ==='Today'){
-	    	var currentDate = moment(new Date()).format("YYYY-MM-DD");
-	    	condition += " AND DATETIME LIKE '" + currentDate + "%'";
-	    	
-	    }else if(dateCategory==='custom'){
-	    	    if(req.body.startDate){
-	    	     var startDateDetails = req.body.startDate.split('/');
-	    	     var day = startDateDetails[0];
-	    	     var month=startDateDetails[1];
-	    	     var year = startDateDetails[2];
-	    		 var newStartDate =  year + "-" + month + "-" + day + ' 00:00:00';
-			    }
-	
-	           if(req.body.endDate){
-	    	     var endDateDetails = req.body.endDate.split('/');
-	    	     var day = endDateDetails[0];
-	    	     var month=endDateDetails[1];
-	    	     var year = endDateDetails[2];
-	    		 var newEndDate =  year + "-" + month + "-" + day + ' 23:59:59';
-			    }
-	    
-			 condition += " AND DATETIME BETWEEN  '" + newStartDate + "' AND '" + newEndDate + "'";
-	    }
+        
+       if(dateCategory ==='Today'){
+        var currentDate = moment(new Date()).format("YYYY-MM-DD");
+        condition += " AND DATETIME LIKE '" + currentDate + "%'";
+        
+      }else if(dateCategory==='custom'){
+            if(req.body.startDate){
+             var startDateDetails = req.body.startDate.split('/');
+             var day = startDateDetails[0];
+             var month=startDateDetails[1];
+             var year = startDateDetails[2];
+           var newStartDate =  year + "-" + month + "-" + day + ' 00:00:00';
+          }
+  
+             if(req.body.endDate){
+             var endDateDetails = req.body.endDate.split('/');
+             var day = endDateDetails[0];
+             var month=endDateDetails[1];
+             var year = endDateDetails[2];
+           var newEndDate =  year + "-" + month + "-" + day + ' 23:59:59';
+          }
+      
+       condition += " AND DATETIME BETWEEN  '" + newStartDate + "' AND '" + newEndDate + "'";
+      }
    }
 
-    
     var qry = 'SELECT SUPPORT_ID, SUPPORT_NAME, AUDIO_URL, DATETIME, SENTIMENT, SENTIMENT_SCORE, ANGER, JOY FROM DASH101569.SENTIMENTDATA ' + condition + ' ORDER BY SUPPORT_ID, DATETIME';
     console.log("SEARCH QUERY : " + qry);
-    
+
     conn.query(qry, function (err, rows) {
-      if (err) console.log(err);
+      if (err) { 
+        console.log(err);
+        conn.end();
+        return;
+      }
       else console.log(rows);
       
-       console.log('DB Search functionality Done');
+      console.log('DB Search functionality Done');
 
       conn.close(function () {
       
